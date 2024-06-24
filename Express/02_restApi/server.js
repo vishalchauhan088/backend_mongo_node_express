13b0a04061b6efea3481b5c2106b5a3d92abb809
Vishal Chauhan <vishalchauhan0688@gmail.com>
2024-06-25 02:05:55 +0530

    handled unhandledREjection | process.on() used | closing() the server

--- Express/02_restApi/server.js
+++ Express/02_restApi/server.js
@@ -1,34 +1,39 @@
-const dotenv = require('dotenv').config();
+const dotenv = require("dotenv").config();
 const app = require("./index");
-const PORT = process.env.PORT  || 3000;
-const mongoose = require('mongoose');
-const Tour = require('./Models/tour');
-//console.log(process.env);
+const PORT = process.env.PORT || 3000;
+const mongoose = require("mongoose");
+const Tour = require("./Models/tour");
 
 console.log("starting connection with database !!");
-const uri = process.env.DATABASE_URI.replace('<password>',process.env.DATABASE_PASSWORD);
-// console.log(uri);
-mongoose.connect(uri).then(con =>{
-  //console.log(con.connection);
-  console.log('database connection succesfull');
-}).catch(e=>{
-  console.log("database connection error",e);
-})
+const uri = process.env.DATABASE_URI.replace(
+  "<password>",
+  process.env.DATABASE_PASSWORD
+);
 
-// const testTour = new Tour({
-//   name:"tour 11",
-//   rating:4,
-//   price:300
-// })
-// testTour.save()
-//   .then(doc =>{
-//     console.log("Document successfully saved to data base ",doc);
-//   })
-//   .catch(e=>{
-//     console.log("error while saving the testTour",e.message);
-//   })
+mongoose.connect(uri).then((con) => {
+  console.log("database connection succesfull");
+});
+
+// handling in unhandled error event at global level
+// .catch((e) => {
+//   console.log("database connection error", e);
+// });
 
 
-app.listen(PORT, () => {
+
+const server = app.listen(PORT, () => {
   console.log(`app listening on port : ${PORT}`);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log("Error:", err.name, err.message);
+  console.log(
+    "Unhandled REJECTION :-------------shutting down server------------------"
+  );
+
+  //process.exti(1) : abrupt way
+
+  server.close(() => {
+    process.exit(1);
+  });
 });
