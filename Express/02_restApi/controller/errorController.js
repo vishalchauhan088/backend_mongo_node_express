0327af1e0daec4f03caef587bfa8e3ff302e97c7
Vishal Chauhan <vishalchauhan0688@gmail.com>
2024-06-24 22:03:31 +0530

    added production v/s development error handling

--- Express/02_restApi/controller/errorController.js
+++ Express/02_restApi/controller/errorController.js
@@ -1,17 +1,65 @@
 
+
+
+function sendErrorDev (res,err){
+  res.status(err.statusCode).json({
+    status:err.status,
+    error:err,
+    errStack:err.stack,
+    message:err.message
+  })
+}
+
+
+
+function sendErroProd (res,err){
+
+  if(err.isOperational === true){
+    res.status(err.statusCode).json({
+      status:err.status,
+      message:err.message
+    })
+  }
+
+  else {
+    console.log('Something Went Wrong !!!!!!!!!!!!!!!!!!!!!!!!!!!!');
+
+
+    res.status(err.statusCode).json({
+
+      status:err.status,
+      message:'something went very wrong' // send a generic error message: as it may be programming error
+    })
+
+  }
+
+
+
+
+}
+
+
+
+
+
 const errorController = (err,req,res,next)=>{
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || "error";
+
+    if(process.env.NODE_ENV === 'development'){
+      sendErrorDev(res,err);
+    }
+    else if( process.env.NODE_ENV === 'production'){
+      sendErroProd(res,err);
+    }
 
     //console.log(err.stack);
       // we will create custom error handler class with these properties
       // but error.message will be present as in original Error class.
-      console.log("inside global handler")
-      err.statusCode = err.statusCode || 500;
-      err.status = err.status || "error";
+      
+     
   
-      res.status(err.statusCode).json({
-        status:err.status,
-        message:err.message
-      })
+      
 }
 
 module.exports = errorController;
